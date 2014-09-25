ddde6ac08b464fc06a169d3984135d0668596c85
Sergey Vartanov <me@enzet.ru>
2014-09-26 02:04:57 +0400

    Preliminary defect position detection in Python.

--- moire.py
+++ moire.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-File conversion from Moire markup language to other formats.
+File conversion from Moiré markup language to other formats.
 
 Usage: python moire.py -i <input file> -o <output file> -t <format> 
                        -r <rules file>
@@ -12,7 +12,7 @@ Options:
     -f <format> resulted format
     -r <file>   rules file name
 
-This file is a part of Moire project—light markup language.
+This file is a part of Moiré project—light markup language.
 
 Author: Sergey Vartanov (me@enzet.ru)
 
@@ -25,10 +25,10 @@ import re
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-i', dest = 'input')
-parser.add_argument('-o', dest = 'output')
-parser.add_argument('-f', dest = 'format')
-parser.add_argument('-r', dest = 'rules')
+parser.add_argument('-i', dest = 'input', help = 'Moiré input file')
+parser.add_argument('-o', dest = 'output', help = 'output file')
+parser.add_argument('-f', dest = 'format', help = 'output format')
+parser.add_argument('-r', dest = 'rules', help = 'rules file')
 parser.add_argument('-pl', dest = 'print_lexems', action = 'store_true',\
 		help = 'print lexems')
 parser.add_argument('-pp', dest = 'print_preprocessed', action = 'store_true',\
@@ -78,7 +78,7 @@ i = 0
 while i < len(input_file):
 	c = input_file[i]
 	if c == '[' and input_file[i - 1] != '\\':
-		end = input_file.find(' ', i)
+		end = min(input_file.find(' ', i), input_file.find('\n', i))
 		if input_file[i + 1:end] == 'ru':
 			adding = True
 			i = end
@@ -142,17 +142,24 @@ def is_space(c):
 	else:
 		return False
 
-def lexer(tx):
+def lexer(text):
+	'''
+	Parse formatted preprocessed text to a list of lexems
+	'''
 	in_tag = False
 	in_space = True
 	lexems = []
+	positions = []
 	tag_name = ''
 	word = ''
 
-	for char in tx:
+	index = 0
+
+	for char in text:
 		if char == '\\':
 			if word != '':
 				lexems.append(word)
+				positions.append(index)
 			in_tag = True
 			tag_name = '\\'
 		elif char == '{':
@@ -160,14 +167,18 @@ def lexer(tx):
 				in_tag = False
 				if tag_name != '':
 					lexems.append(tag_name)
+					positions.append(index)
 			lexems.append('{')
+			positions.append(index)
 			tag_name = ''
 			word = ''
 		elif char == '}':
 			if word != '':
 				lexems.append(word)
+				positions.append(index)
 			word = ''
 			lexems.append('}')
+			positions.append(index)
 		elif is_space(char):
 			if in_tag:
 				in_tag = False
@@ -179,13 +190,16 @@ def lexer(tx):
 				tag_name += char
 			else:
 				word += char
+		index += 1
 
-	return lexems
+	return lexems, positions
 
-lexems = lexer(input_file)
+lexems, positions = lexer(input_file)
 
 if options.print_lexems:
-	print lexems
+	print str(len(lexems)) + " = " + str(len(positions))
+	print '\033[33m' + str(lexems) + '\033[0m'
+	print '\033[34m' + str(positions) + '\033[0m'
 
 index = 0
 level = 0
@@ -214,7 +228,10 @@ def get_line(line):
 		elif item == '}':
 			level -= 1
 			if level < 0:
-				print 'error'
+				position = positions[index]
+				print 'Lexer error at ' + str(position) + '.'
+				print input_file[position - 10:position + 10]
+				print '          ^'
 				index += 1
 				sys.exit(1)
 			if tag != None:
