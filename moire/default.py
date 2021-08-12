0618c15ed5f61297e4c0dafd46127b24a02411de
Sergey Vartanov <me@enzet.ru>
2021-08-13 05:17:27 +1000

    Remove RTF converter.

--- moire/default.py
+++ moire/default.py
@@ -169,7 +169,7 @@ class DefaultHTML(Default):
     def sc(self, arg: Arguments) -> str:
         return (
             f'<span style="font-variant: small-caps;">{self.parse(arg[0])}'
-            '</span>'
+            f"</span>"
         )
 
     def sub(self, arg: Arguments) -> str:
@@ -282,71 +282,6 @@ class DefaultText(Default):
         return self.parse(arg[0], depth=depth + 1)
 
 
-class DefaultRawText(Default):
-    """
-    Plain text without formatting.
-    """
-
-    name = "Text"
-    extension = "txt"
-
-    escape_symbols = {
-        "<": "&lt;",
-    }
-    block_tags = BLOCK_TAGS
-
-    def body(self, arg: Arguments) -> str:
-        return self.parse(arg[0], inblock=True, depth=1) + "\n"
-
-    def header(self, arg, _):
-        return self.parse(arg[0])
-
-    def list(self, arg: Arguments) -> str:
-        s = ""
-        for item in arg[0]:
-            if isinstance(item, list):
-                s += "  * " + self.parse(item, inblock=True, depth=depth + 1)
-        return s
-
-    def table(self, arg: Arguments) -> str:
-        s = ""
-        for tr in arg[0]:
-            for td in tr:
-                s += " " + self.parse(td, inblock=True, depth=depth + 1) + " |"
-            s += "\n"
-        return s
-
-    def b(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def ref(self, arg: Arguments) -> str:
-        return self.parse(arg[1])
-
-    def i(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def size(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def strike(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def sc(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def sub(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def super(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def text(self, arg: Arguments) -> str:
-        return self.parse(arg[0]) + "\n\n"
-
-    def u(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-
 class DefaultMarkdown(Default):
     """
     Markdown.
@@ -355,8 +290,6 @@ class DefaultMarkdown(Default):
     name = "Markdown"
     extensions = ["md", "markdown"]
     block_tags = BLOCK_TAGS
-
-    level = 0
 
     def __init__(self):
         super().__init__()
@@ -373,17 +306,13 @@ class DefaultMarkdown(Default):
         )
 
     def header(self, arg: Arguments, number: int):
-        s = ""
         if number == 1:
             parsed: str = self.parse(arg[0])
-            s += parsed + "\n" + "=" * len(parsed)
-        elif number == 2:
+            return parsed + "\n" + "=" * len(parsed)
+        if number == 2:
             parsed: str = self.parse(arg[0])
-            s += parsed + "\n" + "-" * len(parsed)
-        else:
-            wrapper: str = number * "#"
-            s += f"{wrapper} {self.parse(arg[0])} {wrapper}"
-        return s
+            return parsed + "\n" + "-" * len(parsed)
+        return f"{number * '#'} {self.parse(arg[0])} {number * '#'}"
 
     def list(self, arg: Arguments) -> str:
         self.list_level += 1
@@ -456,8 +385,6 @@ class DefaultWiki(Default):
     name = "Wiki"
     extensions = ["wiki"]
     block_tags = BLOCK_TAGS
-
-    level = 0
 
     def block(self, arg: Arguments) -> str:
         return self.parse(arg[0], inblock=True)
@@ -536,10 +463,11 @@ class DefaultWiki(Default):
         return ">" + self.parse(arg[0]) + ""
 
 
-# TeX.
-
-
 class DefaultTeX(Default):
+    """
+    TeX syntax.
+    """
+
     name = "Tex"
     extension = "tex"
 
@@ -547,6 +475,9 @@ class DefaultTeX(Default):
         "_": "\\_",
     }
     block_tags = BLOCK_TAGS
+    headers: List[str] = [
+        "section", "subsection", "subsubsection", "paragraph", "subparagraph"
+    ]  # fmt: skip
 
     def body(self, arg: Arguments) -> str:
         s = """\\documentclass[twoside,psfig]{article}:
@@ -564,7 +495,7 @@ class DefaultTeX(Default):
         return s
 
     def title(self, arg: Arguments) -> str:
-        s = "\\title{" + self.parse(arg[0]) + "}"
+        s = f"\\title{{{self.parse(arg[0])}}}"
         s += (
             "\\vspace{12em}\\begin{center}{\\huge "
             + self.parse(arg[0])
@@ -573,18 +504,9 @@ class DefaultTeX(Default):
         return s
 
     def header(self, arg, number):
-        if number == 1:
-            return "\\section{" + self.parse(arg[0]) + "}"
-        if number == 2:
-            return "\\subsection{" + self.parse(arg[0]) + "}"
-        if number == 3:
-            return "\\subsubsection{" + self.parse(arg[0]) + "}"
-        if number == 4:
-            return "\\paragraph{" + self.parse(arg[0]) + "}"
-        if number == 5:
-            return "\\subparagraph{" + self.parse(arg[0]) + "}"
-        if number == 6:
-            return "" + self.parse(arg[0]) + ""
+        if number < 6:
+            return f"\\{self.headers[number - 1]}{{{self.parse(arg[0])}}}"
+        return self.parse(arg[0])
 
     def table(self, arg: Arguments) -> str:
         s = "\\begin{table}[h]\n\\begin{center}\n\\begin{tabular}{|"
@@ -617,7 +539,7 @@ class DefaultTeX(Default):
         s = "\\begin{itemize}\n"
         for item in arg[0]:
             if isinstance(item, list):
-                s += "\\item " + self.parse(item) + "\n\n"
+                s += f"\\item {self.parse(item)}\n\n"
         s += "\\end{itemize}\n"
         return s
 
@@ -625,7 +547,7 @@ class DefaultTeX(Default):
         s = "\\begin{ordered}\n"
         for item in arg[0]:
             if isinstance(item, list):
-                s += "\\item " + self.parse(item[0]) + "\n\n"
+                s += f"\\item {self.parse(item[0])}\n\n"
         s += "\\end{ordered}\n"
         return s
 
@@ -651,7 +573,7 @@ class DefaultTeX(Default):
         return s
 
     def b(self, arg: Arguments) -> str:
-        return "{\\bf " + self.parse(arg[0]) + "}"
+        return f"{{\\bf {self.parse(arg[0])}}}"
 
     @staticmethod
     def br(_: Arguments) -> str:
@@ -726,12 +648,6 @@ class DefaultTeX(Default):
     def text(self, arg: Arguments) -> str:
         return self.parse(arg[0]) + "\n\n"
 
-    def tr(self, arg: Arguments) -> str:
-        return "" + self.parse(arg[0]) + "|"
-
-    def td(self, arg: Arguments) -> str:
-        return "| " + self.parse(arg[0]) + ""
-
     def m(self, arg: Arguments) -> str:
         return "{\\tt " + self.parse(arg[0]) + "}"
 
@@ -740,209 +656,6 @@ class DefaultTeX(Default):
 
     def quote(self, arg: Arguments) -> str:
         return "" + self.parse(arg[0]) + ""
-
-
-# RTF.
-
-
-class DefaultRTF(Default):
-    name = "RTF"
-
-    def escape(self, text: str) -> str:
-        result = ""
-        for c in text:
-            if c == "~":
-                result += "\\~"
-            elif ord(c) <= 128:
-                result += c
-            else:
-                result += "\\u" + str(ord(c)) + "  "
-        return result
-
-    def block(self, arg: Arguments) -> str:
-        return self.parse(arg[0], inblock=True)
-
-    def body(self, arg: Arguments) -> str:
-        status["levels"] = [0, 0, 0, 0, 0, 0, 0]
-        status["bookindex"] = 0
-        status["books"] = {}
-        s = """{\\rtf0\\ansi\\deff0\n{\\*\\listtable{\\list\\listtemplateid1
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u8226 ?;}\\li720}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u9702 ?;}\\li1080}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u9642 ?;}\\li1440}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u8226 ?;}\\li1800}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u9702 ?;}\\li2160}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u9642 ?;}\\li2520}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u8226 ?;}\\li2880}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u9702 ?;}\\li3240}
-{\\listlevel\\levelnfc23{\\leveltext \\'01\\u9642 ?;}\\li3600}\\listid1}}
-{\\listoverridetable{\\listoverride\\listid1\\ls1}}
-{\\fonttbl{\\f1 Courier 10 Pitch;}{\\f2 Arial;}{\\f3 Times New Roman;}}\\fs20"""
-        s += "{\\f3 " + self.parse(arg[0], inblock=True) + "}"
-        s += "\n}"
-        return s
-
-    def title(self, arg: Arguments) -> str:
-        return (
-            "\\par\\pard \\qc\\b\\sb346\\sa173{\\f2{\\fs32 "
-            + self.parse(arg[0])
-            + "  \\fs20}}\\b0\n"
-        )
-
-    def table(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def list(self, arg: Arguments) -> str:
-        s = ""
-        for item in arg[0]:
-            if isinstance(item, list):
-                lc = 0  # FIXME
-                lc += 1
-                s += self.parse(item) + " "
-                lc -= 1
-                if lc == 0:
-                    s += "\\par\\pard"
-        return s
-
-    def header(self, arg, number):
-        if number == 1:
-            status["levels"][1] += 1
-            status["levels"][2] = 0
-            status["levels"][3] = 0
-            level = status["levels"][1]
-            return (
-                "\\i\\b\\sb346\\sa173{\\f2\\fs22 "
-                + str(level)
-                + ". "
-                + self.parse(arg[0])
-                + "\\fs20}\\b0\\i0\n"
-            )
-        elif number == 2:
-            status["levels"][2] += 1
-            status["levels"][3] = 0
-            level = (
-                str(status["levels"][1]) + "." + str(status["levels"][2]) + " "
-            )
-            return (
-                "\\b\\par\\pard\\sb346\\sa173{\\fs22 "
-                + level
-                + self.parse(arg[0])
-                + "\\fs20}\\b0\n"
-            )
-        elif number == 3:
-            status["levels"][3] += 1
-            level = (
-                str(status["levels"][1])
-                + "."
-                + str(status["levels"][2])
-                + "."
-                + str(status["levels"][3])
-                + " "
-            )
-            return (
-                "\\parb\\par\\pard\\sb346\\sa173{\\fs20 "
-                + level
-                + self.parse(arg[0])
-                + "\\fs20}\\b0\\i0\n"
-            )
-        elif number == 4:
-            return (
-                "\\parpar\\pard\\sb346\\sa173{\\fs20 "
-                + self.parse(arg[0])
-                + "\\fs20}\\b0\n"
-            )
-        elif number == 5:
-            return (
-                "\\b\\sb346\\sa173{\\fs20 "
-                + self.parse(arg[0])
-                + "\\fs20}\\b0\n"
-            )
-        elif number == 6:
-            return (
-                "\\b\\sb346\\sa173{\\fs20 "
-                + self.parse(arg[0])
-                + "\\fs20}\\b0\n"
-            )
-
-    def center(self, arg: Arguments) -> str:
-        return "\\qc" + self.parse(arg[0])
-
-    def left(self, arg: Arguments) -> str:
-        return "\\ql" + self.parse(arg[0])
-
-    def right(self, arg: Arguments) -> str:
-        return "\\qr" + self.parse(arg[0])
-
-    def b(self, arg: Arguments) -> str:
-        return "\n\\b " + self.parse(arg[0]) + "\\b0\n"
-
-    def code(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    @staticmethod
-    def font(_: Arguments) -> str:
-        return ""
-
-    def ref(self, arg: Arguments) -> str:
-        return (
-            '{\\field{\\*\\fldinst{HYPERLINK  "'
-            + self.parse(arg[0])
-            + '"}}{\\fldrslt{\\u1  '
-            + self.parse(arg[-1])
-            + "\n}}}"
-        )
-
-    def i(self, arg: Arguments) -> str:
-        return "\\i " + self.parse(arg[0]) + "\\i0\n"
-
-    def math(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def ignore(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def image(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def ordered(self, arg: Arguments) -> str:
-        return "\\levelnfc0\\list " + self.parse(arg[0]) + " \\list0"
-
-    def s(self, arg: Arguments) -> str:
-        return "\\strike " + self.parse(arg[0]) + "\\strike0"
-
-    def sc(self, arg: Arguments) -> str:
-        return "\\scaps " + self.parse(arg[0]) + "\\scaps0\n"
-
-    def size(self, arg: Arguments) -> str:
-        return "\\fs" + str(int(self.parse(arg[0])) * 2) + self.parse(arg[1])
-
-    def strike(self, arg: Arguments) -> str:
-        return self.parse(arg[0])
-
-    def sub(self, arg: Arguments) -> str:
-        return "\\sub " + self.parse(arg[0]) + "\\nosupersub\n"
-
-    def super(self, arg: Arguments) -> str:
-        return "\\super " + self.parse(arg[0]) + "\\nosupersub\n"
-
-    def tr(self, arg: Arguments) -> str:
-        return self.parse(arg[0]) + "|"
-
-    def td(self, arg: Arguments) -> str:
-        return "| " + self.parse(arg[0])
-
-    def m(self, arg: Arguments) -> str:
-        return "{\\f1 " + self.parse(arg[0]) + "}"
-
-    def u(self, arg: Arguments) -> str:
-        return "\\ul " + self.parse(arg[0]) + "\\ul0\n"
-
-    def text(self, arg: Arguments) -> str:
-        return "\\par\\pard\\qj" + self.parse(arg[0]) + "\n"
-
-    @staticmethod
-    def br(_: Arguments) -> str:
-        return "\\par\\pard"
 
 
 if __name__ == "__main__":
