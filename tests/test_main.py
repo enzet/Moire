cff9c3d815ed34f3c1e302cd2cd226e082297d77
Sergey Vartanov <me@enzet.ru>
2021-08-13 17:31:14 +1000

    Add tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,33 @@
 from moire.moire import Moire, Tag
+from typing import Any
 
 
 converter: Moire = Moire()
 
 
+def check(moire_code: str, intermediate_representation: Any) -> None:
+    assert converter.full_parse(moire_code) == intermediate_representation
+
+
+def test_text() -> None:
+    check("text", ["text"])
+
+
 def test_tag() -> None:
-    assert converter.full_parse("\\b{text}") == [Tag("b", [["text"]])]
+    check("\\tag{text}", [Tag("tag", [["text"]])])
 
 
-def test_simple() -> None:
-    assert converter.full_parse("text") == ["text"]
+def test_text_and_tag() -> None:
+    check("\\tag{text}text", [Tag("tag", [["text"]]), "text"])
+
+
+def test_text_and_space() -> None:
+    check(" text ", [" text "])
+
+
+def test_text_and_spaces() -> None:
+    check("  text  ", ["  text  "])
+
+
+def test_tag_and_space() -> None:
+    check("\\tag {text}", [Tag("tag", [["text"]])])
