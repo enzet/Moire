b859e9084b70e2084ba7bc2051eb14b1fcebc3cf
Sergey Vartanov <me@enzet.ru>
2024-07-30 22:40:38 +0400

    Add test for Markdown

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -2,6 +2,8 @@
 Tests for Markdown.
 """
 
+from textwrap import dedent
+
 from moire.default import DefaultMarkdown
 
 __author__ = "Sergey Vartanov"
@@ -12,8 +14,21 @@ converter: DefaultMarkdown = DefaultMarkdown()
 
 
 def check(code: str, result: str):
-    assert converter.convert(code, wrap=False, in_block=False) == result
+    assert converter.convert(code, wrap=False) == result
 
 
 def test_markdown() -> None:
     check("\\m {text}", "`text`")
+
+
+def test_markdown_list() -> None:
+    check(
+        "List: \\list {item 1,} {item 2.}",
+        dedent(
+            """\
+            List: 
+              * item 1,
+              * item 2.
+            """  # noqa: W291
+        )[:-1],
+    )
